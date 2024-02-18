d117d970335c8bf3fc93dbdac89f5e8c3b594793
Jessica Cupo <jccupo@gmail.com>
2024-02-17 19:10:19 -0500

    updated knexfile

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL="postgres://fmhtvhgo:Cz3oJZZvwJRxWv59Cl2-j6OcT6XaDUXi@bubble.db.elephantsql.com/fmhtvhgo",
+  DATABASE_URL_DEVELOPMENT="postgres://wfpkfehc:NFqHGImGTe8lWttfzDE_OwA1hJ28v2eb@bubble.db.elephantsql.com/wfpkfehc",
+  DATABASE_URL_TEST="postgres://jpblgbic:3w9P8gUToIoOd5Sd94JDCDutLih9L_XX@bubble.db.elephantsql.com/jpblgbic",
+  DATABASE_URL_PREVIEW="postgres://ojntndzl:J1E67FVRanFt5LNeBVAfj-myjHEjh06m@bubble.db.elephantsql.com/ojntndzl",  
   DEBUG,
 } = process.env;
 
